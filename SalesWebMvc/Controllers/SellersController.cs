244e77bb97da078f0dbee849300f08ee216c231e
Paulo H Sousa <phsaraujo54@gmail.com>
2021-01-01 22:01:30 -0300

    Seller flow - List and Create

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Seller> sellers = _sellerService.FindAll();
+
+            return View(sellers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
